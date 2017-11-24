8a1866c39f9181157f01437007aedcb21806398c
Kaiyuan Hou <khou2020@outlook.com>
2017-11-24 13:36:12 -0600

    add profiling.c

--- runconfig_cori.sh
+++ runconfig_cori.sh
@@ -1,9 +1,8 @@
-./configure --prefix=/path/to/install \
+./configure --prefix=/global/homes/k/khl7265/local/ncmpi_eval \
                 cross_compiling="yes" \
                 CFLAGS="-fast -no-ipo" CXXFLAGS="-fast -no-ipo" \
                 FFLAGS="-fast -no-ipo"  FCFLAGS="-fast -no-ipo" \
                 TESTMPIRUN="srun -n NP" TESTSEQRUN="srun -n 1" \
                 TESTOUTDIR="$SCRATCH" \
 		--disable-shared --disable-debug \
-                --enable-profiling --enable-dwdriver \
-		--prefix=/global/homes/k/khl7265/local/ncmpi_eval
+                --enable-profiling --enable-dwdriver 
